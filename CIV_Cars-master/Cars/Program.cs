8bdc3ba699ff49e9cd56b4fcbe17b04d47ca7f81
(Charlie)YanzuoGe <42125596+geyanzuo@users.noreply.github.com>
2019-05-16 17:05:05 +1000

    Add files via upload

---
+++ CIV_Cars-master/Cars/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars {
+    class Program {
+        static void Main(string[] args) {
+            // Add a driver attribute to the Car -> Driver is of type Passenger
+            // In the Main add a passenger named Anne Clements, I, 1970
+            // add a driver named Dwayne Johnson, M, 1960
+            // Make the car accelerate to 100
+
+            // Print out:
+            //   Car's Make, Model and Speed
+            //   Driver's name, Sex and Age
+            
+
+            Car c1 = new Car(3500, "Nissan", "370Z");
+            c1.Accelerate(100);
+
+            Passenger zali = new Passenger("Zali", 'F', 2003);
+            Passenger anne = new Passenger("Anne Clement",'I', 1970);
+            Passenger dwayne = new Passenger("Dwayne", 'M', 1960);
+
+            c1.AddPassenger(zali);
+
+
+            Console.WriteLine(c1.Make);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Car {
+        public int EngineCapacity;
+        public string Make;
+        public string Model;
+        public int Speed;
+        public List<Passenger> Passengers;
+        public List<Passenger> Drivers;
+
+        public Car(int engineCapacity, string make, string model) {
+            EngineCapacity = engineCapacity;
+            Make = make;
+            Model = model;
+            Speed = 0;
+            Passengers = new List<Passenger>();
+            Drivers = new List<Passenger>();
+        }
+
+        public void Accelerate(int amount) {
+            Speed += amount;
+        }
+
+        public void Decelerate(int amount) {
+            if (Speed >= amount) {
+                Speed -= amount;
+            }
+        }
+
+        public void AddPassenger(Passenger p) {
+            Passengers.Add(p);
+        }
+    }
+
+    public class Passenger {
+        public string Name;
+        public char Sex;
+        public int YearOfBirth;
+
+        public Passenger(string name, char sex, int yearOfBirth) {
+            Name = name;
+            Sex = sex;
+            YearOfBirth = yearOfBirth;
+        }
+
+        public int CalcAge(int CurrentYear) {
+            return CurrentYear - YearOfBirth;
+        }
+
+        public string GetInfo() {
+            return $"{Name} \t {Sex}";
+        }
+    }
+}
